07a76e3211698a2d3a8423b3f445b7a540fd1474
qhh <908102241@qq.com>
2021-08-22 21:13:56 +0800

    第三周作业

---
+++ week03/week03Code/src/main/java/com/geektime/week03Code/filter/ProxyBizFilter.java
@@ -0,0 +1,16 @@
+package com.geektime.week03Code.filter;
+
+import io.github.kimmking.gateway.filter.HttpRequestFilter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class ProxyBizFilter implements HttpRequestFilter {
+
+    @Override
+    public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+    	
+        fullRequest.headers().set("proxyBizFilter", "netty");
+        
+    }
+
+}
